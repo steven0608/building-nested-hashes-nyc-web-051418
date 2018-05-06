78462f3823da6dacf1f59da621f8bdf660bddcbf
Steven Chen <ysteven.chen2@gmail.com>
2018-05-06 04:38:42 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -16,7 +16,7 @@ def third_challenge
       :patriarch => {name:"Lord Capulet",age:"50"
         #your key/value pairs here
         },
-      :matriarch => {name:Lady Capulet,age:"51"
+      :matriarch => {name:"Lady Capulet",age:"51"
         #your key/value pairs here
         },
       :heroine => {name:"Juliet",age:"15",status:"alive"
