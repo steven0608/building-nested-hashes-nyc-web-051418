d86436acf8281cf10bcca6d86a88a71a3700f4e5
Steven Chen <ysteven.chen2@gmail.com>
2018-05-06 04:32:40 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -1,10 +1,15 @@
 def third_challenge
   epic_tragedy = {
    :montague => {
-      :patriarch => {
+      :patriarch => {:name=>{"Lord Montague"},
+      :age=>{"53"}
         #your key/value pairs here
         },
       :matriarch => {
+        :name=>{"Romeo"},
+        :age=>{"15"},
+        :status=>{"alive"}
+        
         #your key/value pairs here
         },
       :hero => {
@@ -14,12 +19,19 @@ def third_challenge
    }, 
    :capulet => {
       :patriarch => {
+        :name=>{"Lord Capulet"},
+        :age=>{"50"}
         #your key/value pairs here
         },
       :matriarch => {
+        :name=>{Lady Capulet},
+        :age=>{"51"}
         #your key/value pairs here
         },
       :heroine => {
+        :name=>{"Juliet"},
+        :age=>{"15"},
+        :status=>{"alive"}
         #your key/value pairs here
         },
       :heroine_friends => []
